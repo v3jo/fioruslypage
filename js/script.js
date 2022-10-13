84341647a07421c2ba8bcdae771b6dc95eb2fe3c
vejo <rodriguez.german412@gmail.com>
2022-10-13 19:33:07 -0300

    correccion de bug

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0);
+    
 })
 
+
+const cabecera = document.querySelector("header"),
+      btn = document.getElementById("btn");
+
+          
+      btn.addEventListener("click", function(){
+        cabecera.classList.toggle("abajo2");
+    })
